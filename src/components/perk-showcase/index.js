b0626d79c34aed011a65263ff180ac794cc20d11
Al <cangzan@gmail.com>
2020-04-14 18:25:05 +0800

    WIP: perk list.

--- src/components/perk-showcase/index.js
+++ src/components/perk-showcase/index.js
@@ -10,8 +10,8 @@ export default function PerkShowcase({ perk }) {
   const pId = selectedPerkIds.find(i => MainStyleIds[primaryStyleId].includes(+i))
 
   return <div className={s.item}>
-    <div className={cn(s[pId])} />
     <div className={cn(s.primary, s[primaryStyleId])} />
+    <div className={cn(s[pId])} />
     <div className={cn(s.sub, s[subStyleId])} />
   </div>;
 }
