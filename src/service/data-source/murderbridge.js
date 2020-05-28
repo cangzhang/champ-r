d2f695a9d882bafdd43d9aa514221b1cd37fa8cd
Al Cheung <cangzan@gmail.com>
2020-05-28 23:31:57 +0800

    WIP: murderbridge.

--- src/service/data-source/murderbridge.js
+++ src/service/data-source/murderbridge.js
@@ -3,6 +3,27 @@ import http from 'src/service/http';
 
 const ApiPrefix = `https://d23wati96d2ixg.cloudfront.net`;
 
+function scorer(winRate, frequency, mean, spread) {
+  if (frequency === 0) {
+    return 0;
+  }
+  const e = 2.71828;
+  let score = 1 / (1 + e ** ((spread / 30) * (mean - frequency)));
+  if (frequency < 0.25) {
+    score *= frequency ** 2;
+  }
+  if (frequency > mean) {
+    return frequency ** (1 / spread) * winRate ** (spread ** 0.1) * score;
+  }
+  return winRate * score;
+}
+
+function scoreGenerator(winRate, frequency, mean, settings) {
+  let adjustedMean = mean || 2.5;
+  adjustedMean = settings && settings.mean ? settings.mean : adjustedMean;
+  return scorer(winRate, frequency, adjustedMean, 100 - settings.ratio);
+}
+
 export default class MurderBridge extends SourceProto {
   constructor() {
     super();
