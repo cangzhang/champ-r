2592f7baa8b76be9d355194ea03c8d1710b4daac
al <cangzan@gmail.com>
2021-04-13 23:31:13 +0800

    change: remove unused.

--- src/share/constants/sources.ts
+++ src/share/constants/sources.ts
@@ -4,10 +4,6 @@ export const Sources = {
   MurderBridge: `murderbridge.com`,
 };
 
-export const AramModes = [Sources.MurderBridge];
-
-export const isAram = (source: string) => AramModes.includes(source);
-
 export const QQServicePrefix = `https://game.gtimg.cn`;
 
 export const QQChampionAvatarPrefix = `${QQServicePrefix}/images/lol/act/img/champion`;
