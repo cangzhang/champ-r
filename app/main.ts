0bedf8155444f092a4124d38299f70f32f4b8265
al <cangzan@gmail.com>
2021-09-23 17:28:49 +0800

    change: replace ts-node.

--- app/main.ts
+++ app/main.ts
@@ -309,9 +309,8 @@ function toggleMainWindow() {
 }
 
 function makeTray() {
-  console.log(__dirname);
   const iconPath = path.join(
-    isDev ? `${__dirname}/../` : process.resourcesPath,
+    isDev ? `${__dirname}/../../` : process.resourcesPath,
     'resources/app-icon.png',
   );
   const icon = nativeImage.createFromPath(iconPath).resize({ width: 24, height: 24 });
