253b11cfdfc2937e90c91b1ab3302e9b32650ca7
qinmaye <qinchi@nuaa.edu.cn>
2021-11-21 21:35:27 +0800

    Remove useless

--- app/utils/lcu.ts
+++ app/utils/lcu.ts
@@ -12,7 +12,7 @@ import {
   IPerkPage,
 } from '@interfaces/commonTypes';
 import { appConfig } from './config';
-import { GamePhase, LcuEvent, LcuMessageType } from '../constants/events';
+import { GamePhase, LcuEvent } from '../constants/events';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const cjk_charset = cjk();
