b92156838d5a1a4d75c6d3ea3c8ed0c1dc9dcc97
al <cangzan@gmail.com>
2022-06-23 16:16:58 +0800

    change: rune window.

---
+++ src/popup.main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>RUNE WINDOW</h1>
+  </React.StrictMode>
+);
