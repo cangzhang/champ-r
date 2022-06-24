1551d9333e776515ef2cbbfcf6e8a05950a88f7e
al <cangzan@gmail.com>
2022-06-24 17:23:22 +0800

    change: ev listener.

--- v2/src-tauri/src/commands.rs
+++ v2/src-tauri/src/commands.rs
@@ -1,4 +1,22 @@
+use tauri::Window;
+
+use crate::Payload;
+
 #[tauri::command]
 pub fn greeting(name: &str) -> String {
     format!("Hello {}", name)
+}
+
+#[tauri::command]
+pub fn emit_msg(window: Window) {
+    println!("window label: {}", window.label());
+    window.open_devtools();
+    window
+        .emit(
+            "event-name",
+            Payload {
+                message: "emit message".into(),
+            },
+        )
+        .unwrap();
 }
--- v2/src-tauri/src/main.rs
+++ v2/src-tauri/src/main.rs
@@ -1,12 +1,19 @@
 #![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 
 pub mod commands;
 pub mod rune_window;
+
+#[derive(Clone, serde::Serialize)]
+pub struct Payload {
+    pub message: String,
+}
 
 fn main() {
     let tray_menu = SystemTrayMenu::new()
@@ -16,24 +23,42 @@ fn main() {
 
     let context = tauri::generate_context!();
     let _app = tauri::Builder::default()
-        .setup(|_app| {
+        .setup(|app| {
+            let _handle = app.handle();
+
+            let main_window = app.get_window("main").unwrap();
+            let _id = main_window.listen("event-name", |event| {
+                println!("got window event-name with payload {:?}", event.payload());
+            });
+
+            // emit the `event-name` event to the `main` window
+            main_window
+                .emit(
+                    "event-name",
+                    Payload {
+                        message: "Tauri is awesome!".into(),
+                    },
+                )
+                .unwrap();
+
             Ok(())
         })
         .system_tray(SystemTray::new().with_menu(tray_menu))
-        .on_system_tray_event(move |app_handle, event| {
-            match event {
-                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                    "toggle_window" => {
-                        let _ = rune_window::toggle_rune_window(app_handle);
-                    }
-                    _ => {
-                        println!("{}", id.as_str());
-                    }
+        .on_system_tray_event(move |app_handle, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "toggle_window" => {
+                    let _ = rune_window::toggle_rune_window(app_handle);
                 }
-                _ => {}
-            }
+                _ => {
+                    println!("{}", id.as_str());
+                }
+            },
+            _ => {}
         })
-        .invoke_handler(tauri::generate_handler![commands::greeting])
+        .invoke_handler(tauri::generate_handler![
+            commands::greeting,
+            commands::emit_msg
+        ])
         .run(context)
         .expect("error while running tauri application");
 }
