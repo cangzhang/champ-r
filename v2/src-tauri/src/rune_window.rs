c0eeb9892d3f48ec80d21914e5236ae03931b20b
al <cangzan@gmail.com>
2022-07-02 12:00:12 +0800

    fix: create window on windows.

--- v2/src-tauri/src/rune_window.rs
+++ v2/src-tauri/src/rune_window.rs
@@ -1,23 +1,26 @@
 use tauri::{AppHandle, Manager, WindowBuilder, WindowUrl};
 
 pub fn toggle(app_handle: &AppHandle) {
-    let w = app_handle.get_window("rune");
-    if let Some(w) = w {
-        let v = w.is_visible().unwrap();
-        if v {
-            w.hide().unwrap();
-        } else {
-            w.show().unwrap();
+    let handle = app_handle.clone();
+    std::thread::spawn(move || {
+        let w = handle.get_window("rune");
+        if let Some(w) = w {
+            let v = w.is_visible().unwrap();
+            if v {
+                w.hide().unwrap();
+            } else {
+                w.show().unwrap();
+            }
+    
+            return ();
         }
 
-        return ();
-    }
-
-    let _w = WindowBuilder::new(app_handle, "rune", WindowUrl::App("rune.html".into()))
+        let _w = WindowBuilder::new(&handle, "rune", WindowUrl::App("rune.html".into()))
         .title("Rune")
         .position(400., 400.)
         .inner_size(400., 540.)
         .skip_taskbar(true)
         .build()
         .unwrap();
+    });
 }
