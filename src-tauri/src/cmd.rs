1eaf53c0ad295aed93cf11a5a50f52d6f21c1cb6
al <cangzan@gmail.com>
2022-08-22 13:24:32 +0800

    change(cmd): add more args.

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -1,18 +1,14 @@
 #[cfg(target_os = "windows")]
 pub fn get_commandline() {
     let cmd_str = r#"Get-CimInstance Win32_Process -Filter "name = 'LeagueClientUx.exe'" | Select-Object -ExpandProperty CommandLine"#;
-
-    let child = std::process::Command::new("powershell")
-        .args(["/C", &cmd_str])
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
-        .spawn()
-        .expect("[powershell] fail to run `Get-CimInstance`");
-    let output = child.wait_with_output().expect("failed to wait on child");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    let ps = powershell_script::PsScriptBuilder::new()
+        .no_profile(true)
+        .non_interactive(true)
+        .hidden(true)
+        .print_commands(false)
+        .build();
+    let output = ps.run(&cmd_str).unwrap();
+    println!("stdout: {}", output.stdout().unwrap());
 }
 
 #[cfg(not(target_os = "windows"))]
