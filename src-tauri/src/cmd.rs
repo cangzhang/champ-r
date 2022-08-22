e5e75cd70de2e697e4c7d6aecb26a7e64a5aae9a
al <cangzan@gmail.com>
2022-08-22 12:38:48 +0800

    change(cmd): pipe output.

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -1,9 +1,12 @@
-// #[cfg(target_os = "windows")]
+#[cfg(target_os = "windows")]
 pub fn get_commandline() {
-    let cmd_str = r#"Get-CimInstance Win32_Process -Filter "name = 'LeagueClientUx.exe'" | Select CommandLine | ConvertTo-Json"#;
+    let cmd_str = r#"Get-CimInstance Win32_Process -Filter "name = 'LeagueClientUx.exe'" | Select-Object -ExpandProperty CommandLine"#;
 
     let child = std::process::Command::new("powershell")
         .args(["/C", &cmd_str])
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
         .spawn()
         .expect("[powershell] fail to run `Get-CimInstance`");
     let output = child.wait_with_output().expect("failed to wait on child");
@@ -12,8 +15,10 @@ pub fn get_commandline() {
     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
 
-// #[cfg(not(target_os = "windows"))]
-// pub fn get_commandline() {}
+#[cfg(not(target_os = "windows"))]
+pub fn get_commandline() {
+    println!("[cmd::get_commandline] not implemented");
+}
 
 #[cfg(test)]
 mod tests {
