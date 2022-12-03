d8b74f2d1cb0537b4b0a98a627acc36c1b732223
al <cangzan@gmail.com>
2022-12-04 01:09:47 +0800

    fix: import component.

--- src/views/Toolbar/Toolbar.tsx
+++ src/views/Toolbar/Toolbar.tsx
@@ -1,8 +1,7 @@
 import { appWindow } from '@tauri-apps/api/window';
 
-import { IconMinus, IconX } from '@tabler/icons';
-import { Avatar } from '@nextui-org/react';
 import cn from 'classnames';
+import { IconMinus, IconX } from '@tabler/icons';
 
 import Logo from '../../assets/icon.png';
 
@@ -12,7 +11,7 @@ export function Toolbar() {
   return (
     <div data-tauri-drag-region className={s.titlebar}>
       <div className={s.appName}>
-        <Avatar bordered zoomed src={Logo} size={'sm'}/>
+        <img src={Logo}/>
         <span className={s.name}>ChampR</span>
       </div>
       
