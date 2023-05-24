38a9302f0c3eac65e706df9fdb0b498c7b42b256
al <cangzan@gmail.com>
2023-05-24 17:42:59 +0800

    change: run as admin

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ extern crate embed_resource;
 
 fn main() {    
     #[cfg(target_os = "windows")]
-    embed_resource::compile("champr-manifest.rc");
+    embed_resource::compile("champr-manifest.rc", embed_resource::NONE);
 }
