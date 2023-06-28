84079536fd38a8f2f0525cb8147464a454bbdbf1
al <cangzan@gmail.com>
2023-06-28 21:52:27 +0800

    change: add icon

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
+#[cfg(windows)]
 extern crate embed_resource;
+#[cfg(windows)]
+extern crate winres;
 
-fn main() {    
+fn main() {
     #[cfg(target_os = "windows")]
     embed_resource::compile("champr-manifest.rc", embed_resource::NONE);
+
+    if std::env::var("PROFILE").unwrap() == "release" {
+        if cfg!(target_os = "windows") {
+            let mut res = winres::WindowsResource::new();
+            res.set_icon("assets/icon@2x_r.ico")
+                .set("InternalName", "CHAMPR.EXE");
+            res.compile().unwrap();
+        }
+    }
 }
