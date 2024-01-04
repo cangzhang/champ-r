2faba594cb456380912ba7255ea05708d9756b55
Al <cangzan@gmail.com>
2024-01-04 20:50:07 +0800

    fix build

--- crates/source_gui/build.rs
+++ crates/source_gui/build.rs
@@ -5,13 +5,13 @@ extern crate winres;
 
 #[cfg(target_os = "windows")]
 fn main() {
-    if std::env::var("PROFILE").unwrap() == "release" {
-        embed_resource::compile("champr-manifest.rc", embed_resource::NONE);
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/icon@2x_r.ico")
-            .set("InternalName", "CHAMPR.EXE");
-        res.compile().unwrap();
-    }
+    // if std::env::var("PROFILE").unwrap() == "release" {
+    //     embed_resource::compile("champr-manifest.rc", embed_resource::NONE);
+    //     let mut res = winres::WindowsResource::new();
+    //     res.set_icon("assets/icon@2x_r.ico")
+    //         .set("InternalName", "CHAMPR.EXE");
+    //     res.compile().unwrap();
+    // }
 }
 
 #[cfg(not(target_os = "windows"))]
