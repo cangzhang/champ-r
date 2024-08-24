f0151b3bf26c11c4bb8bf49d2636c229bd30efc2
Al <cangzan@gmail.com>
2024-08-24 18:57:49 +0800

    init vizia

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -4,218 +4,136 @@
 )]
 
 use kv_log_macro::{error, info};
-use slint::{Model, SharedString, VecModel};
-use std::{rc::Rc, time::Duration};
-
 use lcu::{api, cmd, web};
+use std::{rc::Rc, time::Duration};
+use vizia::prelude::*;
 
-slint::include_modules!();
+const STYLE: &str = r#"
 
-#[tokio::main]
-async fn main() -> Result<(), slint::PlatformError> {
-    femme::with_level(femme::LevelFilter::Info);
-
-    slint::slint! {
-        import { Button, VerticalBox, CheckBox, ComboBox } from "std-widgets.slint";
-
-        // https://github.com/slint-ui/slint/discussions/4324
-        export component ChangeObserver {
-            in property <string> value;
-            // first argument is new value; must return second argument
-            pure callback changed(string, float) -> float;
-
-            width: 0;
-            height: 0;
-            visible: false;
-
-            opacity: changed(value, 1);
-        }
-
-        export component RuneWindow inherits Window {
-            title: "Runes";
-            width: 400px;
-            height: 500px;
-            always-on-top: root.on_top;
-
-            in property <int> champion_id;
-            in property <string> lcu_auth;
-            in property <bool> on_top;
-            in-out property <string> source;
-            in property <[string]> source_list;
-
-            pure callback refetch_data(string, string);
-
-            VerticalBox {
-                spacing: 8px;
-
-                Text {
-                    height: 20px;
-                    text: root.champion_id;
-                }
-
-                combo := ComboBox {
-                    width: self.preferred-width;
-                    height: self.preferred-height;
-                    model: root.source_list;
-                    current-index: 0;
-                    current-value: root.source;
-                }
-
-                // ChangeObserver {
-                //     value: root.champion_id;
-                //     changed => {
-                //         root.refetch_data(root.champion_id, combo.current-value);
-                //         0.
-                //     }
-                // }
-                ChangeObserver {
-                    value: root.champion_id + combo.current-value;
-                    changed => {
-                        root.refetch_data(root.champion_id, combo.current-value);
-                        0.
-                    }
-                }
-            }
-        }
+    .modal {
+        space: 1s;
+        child-space: 8px;
+        child-left: 1s;
+        child-right: 1s;
+        background-color: white;
+        border-radius: 3px;
+        border-width: 1px;
+        border-color: #999999;
+        outer-shadow: 0 3 10 #00000055;
+        overflow: visible;
+        child-space: 10px;
+        height: auto;
     }
 
-    let window = AppWindow::new()?;
-    let rune_window = RuneWindow::new()?;
+    .modal>vstack>label {
+        width: auto;
+        height: auto;
+        space: 5px;
+        child-space: 1s;
+    }
 
-    let _weak_rune_win = rune_window.as_weak();
-    rune_window.on_refetch_data(move |cid, source| {
-        if source.is_empty() {
-            return;
-        }
+    .modal button {
+        border-radius: 3px;
+        child-space: 1s;
+    }
 
-        info!("[rune_window] refetch data for {}, {}", cid, source);
-        tokio::spawn(async move {
-            let champion_id: i64 = cid.to_string().parse().unwrap_or_default();
-            let runes = web::list_builds_by_id(&source.to_string(), champion_id).await;
-            match runes {
-                Ok(runes) => {
-                    info!("[rune_window]: fetched runes {:?}", runes);
+    .modal hstack {
+        col-between: 20px;
+        size: auto;
+    }
+"#;
+
+#[derive(Lens)]
+pub struct AppData {
+    is_saved: bool,
+    show_dialog: bool,
+}
+
+impl Model for AppData {
+    fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
+        event.map(|window_event, meta| {
+            // Intercept WindowClose event to show a dialog if not 'saved'.
+            if let WindowEvent::WindowClose = window_event {
+                if !self.is_saved {
+                    self.show_dialog = true;
+                    meta.consume();
                 }
-                Err(err) => {
-                    error!("[rune_window]: failed to fetch runes: {:?}", err);
-                }
-            };
+            }
         });
-    });
 
-    let weak_win = window.as_weak();
-    let weak_rune_win = rune_window.as_weak();
-    tokio::spawn(async move {
-        let sources = web::fetch_sources().await;
-        match sources {
-            Ok(sources) => {
-                info!("fetched source list");
-
-                let list = sources
-                    .iter()
-                    .map(|s| UiSource {
-                        name: s.label.clone().into(),
-                        source: s.value.clone().into(),
-                        checked: false,
-                    })
-                    .collect::<Vec<UiSource>>();
-
-                weak_win
-                    .upgrade_in_event_loop(move |window| {
-                        let ui_list = Rc::new(VecModel::from(list));
-                        window.set_source_list(ui_list.into());
-                        info!("updated sources");
-                    })
-                    .unwrap();
-
-                let list = sources
-                    .iter()
-                    .map(|s| s.value.clone().into())
-                    .collect::<Vec<SharedString>>();
-                weak_rune_win
-                    .upgrade_in_event_loop(move |rune_win| {
-                        let ui_list = Rc::new(VecModel::from(list));
-                        rune_win.set_source_list(ui_list.into());
-                        info!("rune_window: updated sources");
-                    })
-                    .unwrap();
+        event.map(|app_event, _| match app_event {
+            AppEvent::HideModal => {
+                self.show_dialog = false;
             }
-            Err(err) => {
-                error!("Failed to fetch sources: {:?}", err);
+
+            AppEvent::Save => {
+                self.is_saved = true;
             }
+
+            AppEvent::SaveAndClose => {
+                self.is_saved = true;
+                cx.emit(WindowEvent::WindowClose);
+            }
+
+            AppEvent::Cancel => {
+                self.is_saved = false;
+            }
+        });
+    }
+}
+
+pub enum AppEvent {
+    HideModal,
+    Save,
+    SaveAndClose,
+    Cancel,
+}
+
+fn main() -> Result<(), ApplicationError> {
+    femme::with_level(femme::LevelFilter::Info);
+
+    Application::new(|cx| {
+        cx.add_stylesheet(STYLE).expect("Failed to add stylesheet");
+        AppData {
+            is_saved: false,
+            show_dialog: false,
         }
-    });
+        .build(cx);
 
-    let weak_win = window.as_weak();
-    let weak_rune_window = rune_window.as_weak();
-    tokio::spawn(async move {
-        let mut prev_cid: i64 = 0;
+        HStack::new(cx, |cx| {
+            Button::new(cx, |cx| Label::new(cx, "Close"))
+                .on_press(|cx| cx.emit(WindowEvent::WindowClose));
+            Button::new(cx, |cx| Label::new(cx, "Save")).on_press(|cx| cx.emit(AppEvent::Save));
+        })
+        .col_between(Pixels(10.0))
+        .space(Pixels(20.0));
 
-        loop {
-            let cmd_output = cmd::get_commandline();
-            if cmd_output.auth_url.is_empty() {
-                weak_win
-                    .upgrade_in_event_loop(move |window| {
-                        window.set_lcu_running(false);
-                    })
-                    .unwrap();
+        Dialog::new(cx, AppData::show_dialog, |cx| {
+            VStack::new(cx, |cx| {
+                Label::new(cx, "Save before close?")
+                    .width(Stretch(1.0))
+                    .child_space(Stretch(1.0));
+                HStack::new(cx, |cx| {
+                    Button::new(cx, |cx| Label::new(cx, "Save & Close"))
+                        .on_press(|cx| cx.emit(AppEvent::SaveAndClose))
+                        .width(Pixels(120.0))
+                        .class("accent");
 
-                tokio::time::sleep(Duration::from_millis(2500)).await;
-                continue;
-            }
-
-            let auth_url = format!("https://{}", cmd_output.auth_url);
-            if let Ok(champion_id) = api::get_session(&auth_url).await {
-                let cid = if champion_id.is_some() {
-                    champion_id.unwrap()
-                } else {
-                    0
-                };
-                if prev_cid != cid {
-                    info!("rune_window: champion id changed to: {}", cid);
-                    prev_cid = cid;
-                    weak_rune_window
-                        .upgrade_in_event_loop(move |rune_window| {
-                            if cid > 0 {
-                                rune_window.set_champion_id(cid.try_into().unwrap());
-                                let _ = rune_window.show();
-                                rune_window.set_on_top(true);
-                                info!("rune_window: got champion id: {}", cid);
-                            } else {
-                                // rune_window.set_champion("No champion selected".into());
-                                rune_window.set_on_top(false);
-                                // info!("rune_window: no champion selected");
-                            }
-                        })
-                        .unwrap();
-                }
-            }
-
-            weak_win
-                .upgrade_in_event_loop(move |window| {
-                    window.set_lcu_running(true);
+                    Button::new(cx, |cx| Label::new(cx, "Cancel"))
+                        .on_press(|cx| cx.emit(AppEvent::HideModal))
+                        .width(Pixels(120.0));
                 })
-                .unwrap();
-            weak_rune_window
-                .upgrade_in_event_loop(move |rune_window| {
-                    rune_window.set_lcu_auth(cmd_output.auth_url.clone().into());
-                })
-                .unwrap();
-            tokio::time::sleep(Duration::from_millis(2500)).await;
-        }
-    });
-
-    let weak_win = window.as_weak();
-    window.on_apply_builds(move || {
-        let win = weak_win.unwrap();
-        let selected = win
-            .get_source_list()
-            .iter()
-            .filter(|s| s.checked)
-            .map(|s| s.source.into())
-            .collect::<Vec<String>>();
-        info!("Selected sources: {:?}", selected);
-    });
-
-    window.run()
+                .height(Auto);
+            })
+            .size(Auto)
+            .row_between(Pixels(20.0))
+            .height(Auto);
+        })
+        // .on_blur(|cx| cx.emit(AppEvent::HideModal))
+        .width(Auto)
+        .height(Auto)
+        .row_between(Pixels(20.0))
+        .class("modal");
+    })
+    .run()
 }
--- crates/lcu/src/task.rs
+++ crates/lcu/src/task.rs
@@ -1,4 +1,3 @@
-use eframe::egui;
 use std::{
     sync::{Arc, Mutex, RwLock},
     time::Duration,
@@ -9,66 +8,6 @@ use crate::{
     cmd::{self, CommandLineOutput},
     constants::ALL_CHAMPION_IDS,
 };
-
-pub async fn watch_auth_and_champion(
-    ui_ctx: Arc<Mutex<Option<egui::Context>>>,
-    lcu_auth: Arc<RwLock<CommandLineOutput>>,
-    champion_id: Arc<RwLock<Option<i64>>>,
-    random_mode: Arc<Mutex<bool>>,
-) {
-    loop {
-        let mut repaint = false;
-        let enabled_random_mode = *random_mode.lock().unwrap();
-
-        {
-            let cmd_output = cmd::get_commandline();
-            let mut ui_auth = lcu_auth.write().unwrap();
-            if !cmd_output.auth_url.eq(&ui_auth.auth_url) {
-                println!("auth_url: {}", cmd_output.auth_url);
-                *ui_auth = cmd_output;
-                repaint = true;
-            }
-        }
-
-        let auth_url: String = { lcu_auth.read().unwrap().auth_url.clone() };
-        if !auth_url.is_empty() {
-            let full_url = format!("https://{}", auth_url);
-            if let Ok(Some(cid)) = api::get_session(&full_url).await {
-                let cur_id = champion_id.read().unwrap().unwrap_or_default();
-                if cur_id != cid {
-                    if cur_id == 0 && enabled_random_mode {
-                        // do not clear champion id
-                    } else {
-                        *champion_id.write().unwrap() = Some(cid);
-                        repaint = true;
-                    }
-                    println!("current champion id: {}", cid);
-                }
-            } else {
-                if enabled_random_mode {
-                    if champion_id.read().unwrap().is_none() {
-                        *champion_id.write().unwrap() = Some(get_random_champion_id());
-                    }
-                } else {
-                    *champion_id.write().unwrap() = None;
-                }
-
-                repaint = true;
-            }
-        }
-
-        {
-            if repaint {
-                let ui_ctx = ui_ctx.lock().unwrap();
-                if let Some(x) = ui_ctx.as_ref() {
-                    x.request_repaint();
-                }
-            }
-        }
-
-        tokio::time::sleep(Duration::from_millis(2500)).await;
-    }
-}
 
 fn get_random_champion_id() -> i64 {
     use rand::seq::SliceRandom;
