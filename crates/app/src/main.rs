4061cac7de0200a9b6c0ea3a4ed03685930d3184
Al <cangzan@gmail.com>
2024-08-24 21:36:38 +0800

    lcu status

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -4,8 +4,7 @@
 )]
 
 use kv_log_macro::{error, info};
-use lcu::{api, cmd, web};
-use std::{rc::Rc, time::Duration};
+use lcu::{api, cmd, source, web};
 use vizia::prelude::*;
 
 const STYLE: &str = r#"
@@ -43,10 +42,14 @@ const STYLE: &str = r#"
     }
 "#;
 
-#[derive(Lens)]
+#[derive(Lens, Default)]
 pub struct AppData {
     is_saved: bool,
     show_dialog: bool,
+    source_list: Vec<source::SourceItem>,
+    checked_sources: Vec<String>,
+    lcu_running: bool,
+    current_champion_id: i64,
 }
 
 impl Model for AppData {
@@ -78,6 +81,30 @@ impl Model for AppData {
             AppEvent::Cancel => {
                 self.is_saved = false;
             }
+
+            AppEvent::FetchedSources(sources) => {
+                self.source_list = sources.clone();
+            }
+
+            AppEvent::ToggleSource(source_id) => {
+                if self.checked_sources.contains(&source_id) {
+                    self.checked_sources.retain(|id| id.ne(source_id));
+                } else {
+                    self.checked_sources.push(source_id.clone());
+                }
+            }
+
+            AppEvent::UpdateLcuStatus(running) => {
+                self.lcu_running = running.clone();
+
+                if !running {
+                    self.current_champion_id = 0;
+                }
+            }
+
+            AppEvent::UpdateCurrentChampionId(champion_id) => {
+                self.current_champion_id = champion_id.clone();
+            }
         });
     }
 }
@@ -87,53 +114,120 @@ pub enum AppEvent {
     Save,
     SaveAndClose,
     Cancel,
+    FetchedSources(Vec<source::SourceItem>),
+    ToggleSource(String),
+    UpdateLcuStatus(bool),
+    UpdateCurrentChampionId(i64),
 }
 
-fn main() -> Result<(), ApplicationError> {
+#[tokio::main]
+async fn main() -> Result<(), ApplicationError> {
     femme::with_level(femme::LevelFilter::Info);
 
-    Application::new(|cx| {
+    let app = Application::new(|cx| {
         cx.add_stylesheet(STYLE).expect("Failed to add stylesheet");
         AppData {
             is_saved: false,
             show_dialog: false,
+            ..Default::default()
         }
         .build(cx);
 
-        HStack::new(cx, |cx| {
-            Button::new(cx, |cx| Label::new(cx, "Close"))
-                .on_press(|cx| cx.emit(WindowEvent::WindowClose));
-            Button::new(cx, |cx| Label::new(cx, "Save")).on_press(|cx| cx.emit(AppEvent::Save));
-        })
-        .col_between(Pixels(10.0))
-        .space(Pixels(20.0));
+        VStack::new(cx, |cx| {
+            ScrollView::new(cx, 0.0, 0.0, false, true, |cx| {
+                List::new(cx, AppData::source_list, |cx, _, source| {
+                    let val = source.get(cx).value.clone();
+                    let val2 = val.clone();
+                    let checked =
+                        AppData::checked_sources.map(move |sources| sources.contains(&val));
 
-        Dialog::new(cx, AppData::show_dialog, |cx| {
-            VStack::new(cx, |cx| {
-                Label::new(cx, "Save before close?")
-                    .width(Stretch(1.0))
-                    .child_space(Stretch(1.0));
-                HStack::new(cx, |cx| {
-                    Button::new(cx, |cx| Label::new(cx, "Save & Close"))
-                        .on_press(|cx| cx.emit(AppEvent::SaveAndClose))
-                        .width(Pixels(120.0))
-                        .class("accent");
-
-                    Button::new(cx, |cx| Label::new(cx, "Cancel"))
-                        .on_press(|cx| cx.emit(AppEvent::HideModal))
-                        .width(Pixels(120.0));
-                })
-                .height(Auto);
+                    FormControl::new(
+                        cx,
+                        move |cx| {
+                            let val2 = val2.clone();
+                            Checkbox::new(cx, checked).on_toggle(move |cx| {
+                                cx.emit(AppEvent::ToggleSource(val2.clone()));
+                            })
+                        },
+                        &source.get(cx).label,
+                    );
+                });
             })
-            .size(Auto)
-            .row_between(Pixels(20.0))
-            .height(Auto);
+            .height(Stretch(1.))
+            .class("source-list");
+
+            Label::new(
+                cx,
+                AppData::lcu_running.map(|&running| {
+                    if running {
+                        "LCU is running"
+                    } else {
+                        "LCU is not running"
+                    }
+                }),
+            );
         })
-        // .on_blur(|cx| cx.emit(AppEvent::HideModal))
-        .width(Auto)
-        .height(Auto)
-        .row_between(Pixels(20.0))
-        .class("modal");
+        .height(Stretch(1.));
     })
-    .run()
+    .title("ChampR")
+    .inner_size((500, 400));
+
+    let mut proxy = app.get_proxy();
+    tokio::spawn(async move {
+        let sources = web::fetch_sources().await;
+
+        match sources {
+            Ok(sources) => {
+                proxy
+                    .emit(AppEvent::FetchedSources(sources))
+                    .expect("Failed to emit event");
+            }
+            Err(err) => {
+                error!("Failed to fetch sources: {:?}", err);
+            }
+        }
+    });
+
+    let mut proxy = app.get_proxy();
+    tokio::spawn(async move {
+        let mut prev_cid: i64 = 0;
+        let sleep_duration = Duration::from_millis(2500);
+
+        loop {
+            let cmd_output = cmd::get_commandline();
+            if cmd_output.auth_url.is_empty() {
+                proxy
+                    .emit(AppEvent::UpdateLcuStatus(false))
+                    .expect("Failed to emit event");
+                tokio::time::sleep(sleep_duration).await;
+                continue;
+            }
+
+            proxy
+                .emit(AppEvent::UpdateLcuStatus(true))
+                .expect("Failed to emit event");
+            info!("rune_window: auth url: {}", cmd_output.auth_url);
+
+            let auth_url = format!("https://{}", cmd_output.auth_url);
+            if let Ok(champion_id) = api::get_session(&auth_url).await {
+                let cid = if champion_id.is_some() {
+                    champion_id.unwrap()
+                } else {
+                    0
+                };
+                if prev_cid != cid {
+                    info!("rune_window: champion id changed to: {}", cid);
+                    prev_cid = cid;
+                }
+
+                proxy
+                    .emit(AppEvent::UpdateCurrentChampionId(cid))
+                    .expect("Failed to emit event");
+            }
+
+            tokio::time::sleep(sleep_duration).await;
+        }
+    });
+
+    app.run()
 }
