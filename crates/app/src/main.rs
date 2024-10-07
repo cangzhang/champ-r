b4c1514ef42ccebf0a5de8ffe918aea1b884693f
Al <cangzan@gmail.com>
2024-10-07 16:29:28 +0800

    change

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -3,82 +3,78 @@
     windows_subsystem = "windows"
 )]
 
-use arc_swap::ArcSwap;
-use bytes::Bytes;
-use fltk::{
-    app, button::Button, enums, frame::Frame, group::Flex, image, menu, prelude::*, window::Window,
-};
 use kv_log_macro::{error, info};
-use std::{collections::HashMap, env, path::PathBuf, sync::Arc, time::Duration};
+use slint::{SharedString, VecModel, Model};
+use std::{rc::Rc, time::Duration};
 
-use lcu::{api, builds, cmd, source, web};
+use lcu::{api, cmd, web};
 
-#[derive(Default)]
-pub struct AppData {
-    pub source_list: Vec<source::SourceItem>,
-    pub checked_sources: Vec<String>,
-    pub lcu_running: bool,
-    pub current_champion_id: i64,
-    pub lcu_auth_url: String,
-
-    // UI
-    pub tabs: Vec<&'static str>,
-    pub show_rune_window: bool,
-    pub rune_source: String,
-    pub runes: Vec<builds::Rune>,
-    pub builds: Vec<builds::ItemBuild>,
-    pub perks: Vec<api::Perk>,
-    pub rune_images: HashMap<i64, Bytes>,
-}
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    FetchedSources(Vec<source::SourceItem>),
-    ToggleSource(String),
-    UpdateLcuStatus(bool),
-    UpdateCurrentChampionId(i64),
-    SetLcuAuthUrl(String),
-    ToggleRuneWindow(bool),
-    CloseRuneWindow,
-    SetRuneSource(String),
-    UpdateBuilds(Vec<builds::BuildSection>),
-    ReFetchRunes,
-    SetPerks(Vec<api::Perk>),
-    SetRuneImage(Bytes),
-    SetChampionIcon(Bytes),
-    UpdateChampionIconPath(PathBuf),
-}
+slint::include_modules!();
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), slint::PlatformError> {
     femme::with_level(femme::LevelFilter::Info);
 
-    let tmp_folder = env::temp_dir();
+    let window = AppWindow::new()?;
+    let rune_window = RuneWindow::new()?;
 
-    let (s, r) = app::channel::<Message>();
-    let main_app = app::App::default();
-    let mut win = Window::new(0, 0, 400, 300, "ChampR").center_screen();
-    win.make_resizable(true);
+    // let weak_rune_win = rune_window.as_weak();
+    // rune_window.on_refetch_data(move |cid, source| {
+    //     if source.is_empty() {
+    //         return;
+    //     }
 
-    let mut frame = Frame::default_fill();
-    frame.set_label("loading...");
+    //     info!("[rune_window] refetch data for {}, {}", cid, source);
+    //     tokio::spawn(async move {
+    //         let champion_id: i64 = cid.to_string().parse().unwrap_or_default();
+    //         let runes = web::list_builds_by_id(&source.to_string(), champion_id).await;
+    //         match runes {
+    //             Ok(runes) => {
+    //                 info!("[rune_window]: fetched runes {:?}", runes);
+    //             }
+    //             Err(err) => {
+    //                 error!("[rune_window]: failed to fetch runes: {:?}", err);
+    //             }
+    //         };
+    //     });
+    // });
 
-    let s_thread = s.clone();
-    let s_thread_clone = s.clone();
-
-    // shared variables
-    let auth_url = Arc::new(ArcSwap::from(Arc::new(String::new())));
-    let champion_id = ArcSwap::from(Arc::new(0 as i64));
-    let auto_close_rune_window = Arc::new(ArcSwap::from(Arc::new(true)));
-    let source_list = Arc::new(ArcSwap::from(Arc::new(vec![])));
-    let rune_source = Arc::new(ArcSwap::from(Arc::new(String::from("op.gg"))));
-
-    let source_list_clone = Arc::clone(&source_list);
+    let weak_win = window.as_weak();
+    let weak_rune_win = rune_window.as_weak();
     tokio::spawn(async move {
-        match web::fetch_sources().await {
+        let sources = web::fetch_sources().await;
+        match sources {
             Ok(sources) => {
-                source_list_clone.store(Arc::new(sources.clone()));
-                s_thread.send(Message::FetchedSources(sources));
+                info!("fetched source list");
+
+                let list = sources
+                    .iter()
+                    .map(|s| UiSource {
+                        name: s.label.clone().into(),
+                        source: s.value.clone().into(),
+                        checked: false,
+                    })
+                    .collect::<Vec<UiSource>>();
+
+                weak_win
+                    .upgrade_in_event_loop(move |window| {
+                        let ui_list = Rc::new(VecModel::from(list));
+                        window.set_source_list(ui_list.into());
+                        info!("updated sources");
+                    })
+                    .unwrap();
+
+                let list = sources
+                    .iter()
+                    .map(|s| s.value.clone().into())
+                    .collect::<Vec<SharedString>>();
+                weak_rune_win
+                    .upgrade_in_event_loop(move |rune_win| {
+                        let ui_list = Rc::new(VecModel::from(list));
+                        rune_win.set_source_list(ui_list.into());
+                        info!("rune_window: updated sources");
+                    })
+                    .unwrap();
             }
             Err(err) => {
                 error!("Failed to fetch sources: {:?}", err);
@@ -86,168 +82,78 @@ async fn main() {
         }
     });
 
-    let s_rune = s.clone();
-    let auth_url_clone = Arc::clone(&auth_url);
+    let weak_win = window.as_weak();
+    let weak_rune_window = rune_window.as_weak();
     tokio::spawn(async move {
-        // let mut prev_cid: i64 = 0;
-        let sleep_duration = Duration::from_millis(2500);
+        let mut prev_cid: i64 = 0;
 
         loop {
             let cmd_output = cmd::get_commandline();
             if cmd_output.auth_url.is_empty() {
-                tokio::time::sleep(sleep_duration).await;
+                weak_win
+                    .upgrade_in_event_loop(move |window| {
+                        window.set_lcu_running(false);
+                    })
+                    .unwrap();
+
+                tokio::time::sleep(Duration::from_millis(2500)).await;
                 continue;
             }
 
-            let new_auth_url = format!("https://{}", cmd_output.auth_url);
-            auth_url_clone.store(Arc::new(new_auth_url.clone()));
-
-            if let Ok(cid) = api::get_session(&new_auth_url).await {
-                let cid = cid.unwrap_or_default();
-                let prev_cid = champion_id.load();
-                if **prev_cid != cid {
-                    info!("champion id changed to: {}", cid);
-                    champion_id.store(Arc::new(cid));
-                    s_rune.send(Message::UpdateCurrentChampionId(cid));
-                    s_rune.send(Message::ToggleRuneWindow(true));
-                }
-
-                if cid == 0 {
-                    s_rune.send(Message::ToggleRuneWindow(false));
-                }
-            } else {
-                s_rune.send(Message::ToggleRuneWindow(false));
-            }
-
-            tokio::time::sleep(sleep_duration).await;
-        }
-    });
-
-    let mut source_list_el = Flex::default_fill().column();
-    let mut btn = Button::new(0, 0, 50, 300, "show rune window");
-    let acrw = Arc::clone(&auto_close_rune_window);
-    btn.set_callback(move |_| {
-        acrw.store(Arc::new(false));
-        s.send(Message::ToggleRuneWindow(true));
-    });
-    source_list_el.end();
-
-    win.end();
-    win.show();
-
-    let mut rune_win = Window::new(0, 0, 400, 300, "Runes");
-    let mut choice = menu::Choice::default()
-        .with_size(200, 30)
-        .with_label("Select source");
-    // let top_row = Flex::new(0, 0, 300, 100, "").row();
-    let mut champion_frame = Frame::new(0, 20, 100, 100, "");
-    // top_row.end();
-    rune_win.end();
-
-    let auth_url = Arc::clone(&auth_url);
-    let acrw = Arc::clone(&auto_close_rune_window);
-    let source_list_clone = Arc::clone(&source_list);
-
-    while main_app.wait() {
-        if let Some(msg) = r.recv() {
-            match msg {
-                Message::FetchedSources(sources) => {
-                    // pack.clear();
-                    frame.set_label("");
-                    for source in sources.iter() {
-                        let mut button = Button::new(0, 0, 400, 30, Some(source.label.as_str()));
-                        let val = source.value.clone();
-                        button.set_callback({
-                            let val = val.clone();
-                            move |_| {
-                                info!("clicked {}", &val);
-                            }
-                        });
-                        source_list_el.add(&button);
-
-                        // let rune_source = Arc::clone(&rune_source);
-                        let val = source.value.clone();
-                        let s_thread = s_thread_clone.clone();
-                        choice.add(
-                            source.label.as_str(),
-                            enums::Shortcut::None,
-                            menu::MenuFlag::Normal,
-                            move |_| {
-                                let val = val.clone();
-                                // rune_source.store(Arc::new(val.clone()));
-                                s_thread.send(Message::SetRuneSource(val.clone()));
-                            },
-                        );
-                    }
-                    source_list_el.redraw();
-                    // win.redraw();
-                }
-
-                Message::ToggleRuneWindow(show) => {
-                    if show {
-                        rune_win.show();
-                    } else {
-                        let auto_close = acrw.load();
-                        if **auto_close {
-                            rune_win.hide();
-                        }
-                    }
-                }
-
-                Message::UpdateCurrentChampionId(cid) => {
-                    if cid > 0 {
-                        let s = s_thread_clone.clone();
-                        let rune_source = rune_source.load();
-                        let rune_source = (**rune_source).clone();
-                        s.send(Message::SetRuneSource(rune_source));
-
-                        let auth_url = auth_url.load();
-                        let idle = auth_url.is_empty();
-                        if !idle {
-                            let tmp_folder = tmp_folder.clone();
-                            let file_path = tmp_folder.join(format!("champion_{}.png", cid));
-                            if file_path.exists() {
-                                info!("champion icon exists: {cid}");
-                                s.send(Message::UpdateChampionIconPath(file_path));
+            let auth_url = format!("https://{}", cmd_output.auth_url);
+            if let Ok(champion_id) = api::get_session(&auth_url).await {
+                let cid = if champion_id.is_some() {
+                    champion_id.unwrap()
+                } else {
+                    0
+                };
+                if prev_cid != cid {
+                    info!("rune_window: champion id changed to: {}", cid);
+                    prev_cid = cid;
+                    weak_rune_window
+                        .upgrade_in_event_loop(move |rune_window| {
+                            if cid > 0 {
+                                rune_window.set_champion_id(cid.try_into().unwrap());
+                                let _ = rune_window.show();
+                                rune_window.set_on_top(true);
+                                info!("rune_window: got champion id: {}", cid);
                             } else {
-                                tokio::spawn(async move {
-                                    if let Ok(b) =
-                                        lcu::api::get_champion_icon_by_id(&auth_url, cid).await
-                                    {
-                                        info!("fetched champion image by id {cid}");
-                                        if let Err(e) = tokio::fs::write(&file_path, &b).await {
-                                            error!("Failed to save champion icon: {:?}", e);
-                                        }
-                                        s.send(Message::UpdateChampionIconPath(file_path));
-                                    }
-                                });
+                                // rune_window.set_champion("No champion selected".into());
+                                rune_window.set_on_top(false);
+                                // info!("rune_window: no champion selected");
                             }
-                        }
-                    }
+                        })
+                        .unwrap();
                 }
-
-                Message::UpdateChampionIconPath(p) => {
-                    if let Ok(mut img) = image::PngImage::load(p) {
-                        img.scale(48, 48, true, true);
-                        champion_frame.set_image(Some(img));
-                        champion_frame.redraw();
-                    } else {
-                        error!("Failed to create png image from data");
-                    }
-                }
-
-                Message::SetRuneSource(source) => {
-                    rune_source.store(Arc::new(source.clone()));
-                    let idx = source_list_clone
-                        .load()
-                        .iter()
-                        .position(|s| s.value.eq(&source))
-                        .unwrap_or(0);
-                    choice.set_value(idx as i32);
-                }
-
-                _ => {}
             }
+
+            weak_win
+                .upgrade_in_event_loop(move |window| {
+                    window.set_lcu_running(true);
+                })
+                .unwrap();
+            weak_rune_window
+                .upgrade_in_event_loop(move |rune_window| {
+                    rune_window.set_lcu_auth(cmd_output.auth_url.clone().into());
+                })
+                .unwrap();
+            tokio::time::sleep(Duration::from_millis(2500)).await;
         }
-    }
+    });
+
+    let weak_win = window.as_weak();
+    window.on_apply_builds(move || {
+        let win = weak_win.unwrap();
+        let selected = win
+            .get_source_list()
+            .iter()
+            .filter(|s| s.checked)
+            .map(|s| s.source.into())
+            .collect::<Vec<String>>();
+        info!("Selected sources: {:?}", selected);
+    });
+
+    window.show()?;
+    slint::run_event_loop()?;
+    Ok(())
 }
